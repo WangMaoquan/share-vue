9153f5a7b8dd8fdb1983678da6b49b12c677816c
decade <1782976211@qq.com>
2023-10-11 10:04:21 +0800

    fix: reactive 简单响应式

--- realize/reactivity/reactivity.js
+++ realize/reactivity/reactivity.js
@@ -1,4 +1,8 @@
-const reactiveMap = new WeakMap();
+/** utils */
+
+const hasOwn = Object.hasOwn;
+
+/** baseHandlers */
 
 const baseHandlers = {
   get(target, key, reciver) {
@@ -7,14 +11,20 @@ const baseHandlers = {
     } else if (key === '__is_reactive') {
       return true;
     }
-    // todo track
-    return Reflect.get(target, key, reciver);
+    const res = Reflect.get(target, key, reciver);
+    track(target, key);
+    return res;
   },
   set(target, key, value, reciver) {
-    // todo trigger
-    return Reflect.set(target, key, value, reciver);
+    const result = Reflect.set(target, key, value, reciver);
+    trigger(target, key);
+    return result;
   },
 };
+
+/** reactive */
+
+const reactiveMap = new WeakMap();
 
 function createReactiveObject(obj) {
   const targetProxy = reactiveMap.get(obj);
@@ -36,3 +46,66 @@ function reactive(obj) {
   }
   return createReactiveObject(obj);
 }
+
+/** effect */
+
+const targetMap = new WeakMap();
+let activeEffect;
+
+class ReactiveEffect {
+  constructor(fn, scheduler) {
+    this.fn = fn;
+    this.scheduler = scheduler;
+  }
+
+  run() {
+    activeEffect = this;
+    return this.fn();
+  }
+}
+
+function effect(fn, options) {
+  const _effect = new ReactiveEffect(fn);
+  Object.assign(_effect, options);
+  return _effect.run();
+}
+
+function track(target, key) {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()));
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
+  }
+}
+
+function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) {
+    return;
+  }
+  const deps = depsMap.get(key);
+  const effects = [...deps];
+  for (let i = 0; i < effects.length; i++) {
+    effects[i].run();
+  }
+}
+
+/** test */
+
+function test() {
+  const state = reactive({
+    name: 'decade',
+    age: 21,
+  });
+
+  effect(() => console.log(state.age));
+  state.age = 20;
+}
+
+test();
